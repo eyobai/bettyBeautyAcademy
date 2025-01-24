3e5f2f2c8857729d0123fbfa6660a1de6ddbee6d
Eyob <eyobsisayy@gmail.com>
2025-01-24 17:03:18 +0300

    registraton controller added

--- controllers/studentController.js
+++ controllers/studentController.js
@@ -5,10 +5,10 @@ const { createStudent } = require('../models/student');
 const SECRET_KEY = 'your_secret_key'; // Replace with your actual secret key
 
 const register = async (req, res) => {
-  const { name, contactDetails, email, address, password } = req.body;
+  const { firstName, lastName, email, address, password } = req.body;
   const hashedPassword = await bcrypt.hash(password, 10);
 
-  createStudent({ name, contactDetails, email, address, password: hashedPassword }, (err, result) => {
+  createStudent({ firstName, lastName, email, address, password: hashedPassword }, (err, result) => {
     if (err) {
       return res.status(500).json({ error: err.message });
     }
